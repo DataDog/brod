e380f25723f673f677524cfd57f7cf96c1092e53
Christian Carollo <ccarollo@gmail.com>
2011-07-15 14:40:48 -0700

    Changes to support 0.6

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,6 +1,7 @@
 import contextlib
 import itertools
 import struct
+import binascii
 
 import kafka.io
 import kafka.request_type
@@ -12,34 +13,34 @@ class Producer(kafka.io.IO):
 
   def __init__(self, topic, partition=0, host='localhost', port=9092):
     kafka.io.IO.__init__(self, host, port)
-
     self.topic     = topic
     self.partition = partition
-
     self.connect()
+
+  def encode(self, message):
+    """ Encode a :class:`Message` to binary form. """
+
+    # <MAGIC_BYTE: char> <CRC32: int> <PAYLOAD: bytes>
+    print message.payload
+    print message.calculate_checksum()
+    return struct.pack('>B', 0) + \
+           struct.pack('>i', binascii.crc32(message.payload)) + \
+           message.payload
 
   def encode_request(self, messages):
     """ Encode a sequence of :class:`Message <kafka.message>` objects for sending to the broker. """
 
     # encode messages as <LEN: int><MESSAGE_BYTES>
-    encoded = [message.encode() for message in messages]
-    lengths = [len(em) for em in encoded]
-
-    # Build up the struct format.
-    mformat = '>' + ''.join(['i%ds' % l for l in lengths])
-
-    # Flatten the two lists to match the format.
-    message_set = struct.pack(mformat, *list(itertools.chain.from_iterable(zip(lengths, encoded))))
-
-    topic_len = len(self.topic)
-    mset_len  = len(message_set)
-
-    # Create the request as <REQUEST_SIZE: int> <REQUEST_ID: short> <TOPIC: bytes> <PARTITION: int> <BUFFER_SIZE: int> <BUFFER: bytes>
-    pformat   = '>HH%dsii%ds' % (topic_len, mset_len)
-    payload   = struct.pack(pformat, self.PRODUCE_REQUEST_ID, topic_len, self.topic, self.partition, mset_len, message_set)
-
-    return struct.pack('>i%ds' % len(payload), len(payload), payload)
-
+    encoded = [self.encode(message) for message in messages]
+    message_set = ''.join([struct.pack('>I', len(m)) + m for m in encoded])
+    
+    # create the request as <REQUEST_SIZE: int>, <REQUEST_ID: short> <TOPIC: bytes> <PARTITION: int> <BUFFER: bytes>
+    data = struct.pack('>H', self.PRODUCE_REQUEST_ID) + \
+           struct.pack('>H', len(self.topic)) + self.topic + \
+           struct.pack('>I', self.partition) + \
+           struct.pack('>I', len(message_set)) + message_set
+    return struct.pack('>I', len(data)) + data
+  
   def send(self, messages):
     """ Send a :class:`Message <kafka.message>` or a sequence of `Messages` to the Kafka server. """
 
