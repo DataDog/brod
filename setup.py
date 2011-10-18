0845c53428e8c02914213d240e6315a520e8c109
Carlo Cabanilla <carlo@datadoghq.com>
2011-10-18 12:08:36 -0400

    bump the version number to 2.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
   name = 'pykafka',
-  version = '2.4.0',
+  version = '2.4.1',
   license = 'MIT',
   description = open('README.md').read(),
   author = "Datadog, Inc.",
