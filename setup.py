8eb3017ea13a4a8edb240d307c5235766710645a
David Ormsbee <dave@datadoghq.com>
2011-12-15 15:59:50 -0500

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setuptools.setup(
     name = 'brod',
-    version = '0.1.3',
+    version = '0.1.4',
     license = 'MIT',
     description = open('README.md').read(),
     author = "Datadog, Inc.",
